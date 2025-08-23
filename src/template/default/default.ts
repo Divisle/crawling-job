18cc56a6279e604ac6b093048f3d5bc668d0fd8f
PlanHorror <nmtxma2004@gmail.com>
2025-08-23 23:51:12 +0700

    feat: function build block message

--- src/template/default/default.ts
+++ src/template/default/default.ts
@@ -12,5 +12,65 @@ export interface DefaultJobMessageData {
 }
 
 export function buildDefaultJobMessage(data: DefaultJobMessageData) {
-  // Implementation here
+  const blocks: any[] = [];
+  const divider = {
+    type: "divider",
+  };
+  blocks.push({
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: `We found *${data.newJobs.length} new jobs*, *${data.updateJobs.length} updated jobs* and *${data.deleteJobs.length} jobs removed*`,
+    },
+  });
+  if (
+    data.newJobs.length === 0 &&
+    data.updateJobs.length === 0 &&
+    data.deleteJobs.length === 0
+  ) {
+    blocks.push({
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "No job updates found.",
+      },
+    });
+  }
+  if (data.newJobs.length > 0) {
+    blocks.push({
+      type: "header",
+      text: {
+        type: "plain_text",
+        text: "New Jobs",
+        emoji: true,
+      },
+    });
+    data.newJobs.forEach((job) => {
+      blocks.push({
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `*<${job.href}|${job.title}>*\nDepartment: ${job.department}`,
+        },
+      });
+      blocks.push({
+        type: "context",
+        elements: [
+          {
+            type: "image",
+            image_url:
+              "https://api.slack.com/img/blocks/bkb_template_images/tripAgentLocationMarker.png",
+            alt_text: "Location",
+          },
+          {
+            type: "mrkdwn",
+            location: job.location,
+          },
+        ],
+      });
+      blocks.push(divider);
+    });
+  }
+  blocks.push(divider);
+  return blocks;
 }
