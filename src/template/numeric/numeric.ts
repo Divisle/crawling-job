85ed13da70e62a46457fdf638f578482405d4954
PlanHorror <nmtxma2004@gmail.com>
2025-08-25 17:56:48 +0700

    feat: NumericJob interface

---
+++ src/template/numeric/numeric.ts
@@ -0,0 +1,10 @@
+export interface NumericJob {
+  id: string;
+  title: string;
+  location_type: string;
+  address: string;
+  department: string;
+  time: string;
+  href: string;
+  tags: string[];
+}
