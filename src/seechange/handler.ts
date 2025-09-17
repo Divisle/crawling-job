aaaf5c69df680623268695e84436ecfe852f6f35
L <lillustrer012@gmail.com>
2025-09-17 16:15:44 +0700

    refactor: refactor job message (Seechange)

--- src/seechange/handler.ts
+++ src/seechange/handler.ts
@@ -1,82 +1,99 @@
 import { Prisma, PrismaClient } from "@prisma/client";
-import { buildSeeChangeMessage, SeeChangeApiPayload } from "../template";
-import { buildMessage } from "../global";
+import {
+	buildJobMessage,
+	JobMessageData,
+	SeeChangeApiPayload,
+} from "../template";
 import axios from "axios";
 import { SeeChangeRepository } from "./database";
 export class SeeChangeJobHandler {
-  constructor(private db = new SeeChangeRepository(new PrismaClient())) {
-    if (!process.env.SLACK_BOT_TOKEN) {
-      console.log("SLACK_BOT_TOKEN is not defined");
-      return process.exit(1);
-    }
-    if (!process.env.SLACK_FIRST_CHANNEL_ID) {
-      console.log("SLACK_FIRST_CHANNEL_ID is not defined");
-      return process.exit(1);
-    }
-  }
+	constructor(private db = new SeeChangeRepository(new PrismaClient())) {
+		if (!process.env.SLACK_BOT_TOKEN) {
+			console.log("SLACK_BOT_TOKEN is not defined");
+			return process.exit(1);
+		}
+		if (!process.env.SLACK_FIRST_CHANNEL_ID) {
+			console.log("SLACK_FIRST_CHANNEL_ID is not defined");
+			return process.exit(1);
+		}
+	}
 
-  async scrapeJobs(): Promise<Prisma.SeeChangeJobCreateInput[]> {
-    try {
-      const jobData: Prisma.SeeChangeJobCreateInput[] = [];
-      const response: {
-        data: SeeChangeApiPayload;
-      } = await axios.get("https://seechange.bamboohr.com/careers/list");
-      for (const job of response.data.result) {
-        jobData.push({
-          jobId: job.id,
-          title: job.jobOpeningName,
-          city: job.location.city,
-          department: job.departmentLabel,
-          employmentType: job.employmentStatusLabel,
-          state: job.location.state,
-          href: `https://seechange.com/careers/${job.id}`,
-        });
-      }
-      return jobData;
-    } catch (error) {
-      console.error("Error scraping jobs:", error);
-      return [];
-    }
-  }
+	async scrapeJobs(): Promise<Prisma.SeeChangeJobCreateInput[]> {
+		try {
+			const jobData: Prisma.SeeChangeJobCreateInput[] = [];
+			const response: {
+				data: SeeChangeApiPayload;
+			} = await axios.get("https://seechange.bamboohr.com/careers/list");
+			for (const job of response.data.result) {
+				jobData.push({
+					jobId: job.id,
+					title: job.jobOpeningName,
+					city: job.location.city,
+					department: job.departmentLabel,
+					employmentType: job.employmentStatusLabel,
+					state: job.location.state,
+					href: `https://seechange.com/careers/${job.id}`,
+				});
+			}
+			return jobData;
+		} catch (error) {
+			console.error("Error scraping jobs:", error);
+			return [];
+		}
+	}
 
-  async filterData(data: Prisma.SeeChangeJobCreateInput[]) {
-    const filteredData = await this.db.compareData(data);
-    const listDeleteId = [
-      ...filteredData.deleteJobs.map((job) => job.id as string),
-      ...filteredData.updateJobs.map((job) => job.id as string),
-    ];
-    const listCreateData = [
-      ...filteredData.newJobs,
-      ...filteredData.updateJobs.map((job) => {
-        const { id, ...jobData } = job;
-        return jobData;
-      }),
-    ];
-    await this.db.deleteMany(listDeleteId);
-    await this.db.createMany(listCreateData);
-    return filteredData;
-  }
+	async filterData(data: Prisma.SeeChangeJobCreateInput[]) {
+		const filteredData = await this.db.compareData(data);
+		const listDeleteId = [
+			...filteredData.deleteJobs.map((job) => job.id as string),
+			...filteredData.updateJobs.map((job) => job.id as string),
+		];
+		const listCreateData = [
+			...filteredData.newJobs,
+			...filteredData.updateJobs.map((job) => {
+				const { id, ...jobData } = job;
+				return jobData;
+			}),
+		];
+		await this.db.deleteMany(listDeleteId);
+		await this.db.createMany(listCreateData);
+		return filteredData;
+	}
 
-  async sendMessage(data: {
-    newJobs: Prisma.SeeChangeJobCreateInput[];
-    updateJobs: Prisma.SeeChangeJobCreateInput[];
-    deleteJobs: Prisma.SeeChangeJobCreateInput[];
-  }) {
-    const blocks = buildSeeChangeMessage(data);
-    return { blocks, channel: 2 };
-  }
-  static async run() {
-    const handler = new SeeChangeJobHandler();
-    const jobs = await handler.scrapeJobs();
-    const filteredData = await handler.filterData(jobs);
-    if (
-      filteredData.newJobs.length === 0 &&
-      filteredData.updateJobs.length === 0 &&
-      filteredData.deleteJobs.length === 0
-    ) {
-      console.log("No job changes detected.");
-      return { blocks: [] as any[], channel: 0 };
-    }
-    return await handler.sendMessage(filteredData);
-  }
+	async sendMessage(data: {
+		newJobs: Prisma.SeeChangeJobCreateInput[];
+		updateJobs: Prisma.SeeChangeJobCreateInput[];
+		deleteJobs: Prisma.SeeChangeJobCreateInput[];
+	}) {
+		const jobs: JobMessageData[] = data.newJobs.map((jobData) => {
+			return {
+				location: `${jobData.city ? jobData.city : "No city provided"}, ${
+					jobData.state ? jobData.state : "No state provided"
+				}`,
+				title: jobData.title,
+				href: jobData.href,
+			};
+		});
+		const blocks = buildJobMessage(
+			jobs,
+			"Seechange",
+			"https://seechange.com",
+			2,
+		);
+		return { blocks, channel: 2 };
+	}
+	static async run() {
+		const handler = new SeeChangeJobHandler();
+		const jobs = await handler.scrapeJobs();
+		const filteredData = await handler.filterData(jobs);
+		if (
+			filteredData.newJobs.length === 0 &&
+			filteredData.updateJobs.length === 0 &&
+			filteredData.deleteJobs.length === 0
+		) {
+			console.log("No job changes detected.");
+			return { blocks: [] as any[], channel: 0 };
+		}
+		return await handler.sendMessage(filteredData);
+	}
 }
