7ed4db9a393f431f630149ea71c564daf3b69789
L <lillustrer012@gmail.com>
2025-09-17 16:17:25 +0700

    refactor: refactor job message (Safe)

--- src/safe/handler.ts
+++ src/safe/handler.ts
@@ -1,159 +1,171 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import { SafeJobRepository } from "./database";
-import { buildLeverJobMessage } from "../template";
-import { buildMessage } from "../global";
+import {
+	buildJobMessage,
+	buildLeverJobMessage,
+	JobMessageData,
+} from "../template";
 import { Builder, By, WebDriver } from "selenium-webdriver";
 import { Options } from "selenium-webdriver/chrome";
+import { join } from "path";
 
 export class SafeJobHandler {
-  private driver: WebDriver;
+	private driver: WebDriver;
 
-  constructor(private db = new SafeJobRepository(new PrismaClient())) {
-    if (!process.env.SLACK_BOT_TOKEN) {
-      console.log("SLACK_BOT_TOKEN is not defined");
-      return process.exit(1);
-    }
-    if (!process.env.SLACK_FIRST_CHANNEL_ID) {
-      console.log("SLACK_FIRST_CHANNEL_ID is not defined");
-      return process.exit(1);
-    }
+	constructor(private db = new SafeJobRepository(new PrismaClient())) {
+		if (!process.env.SLACK_BOT_TOKEN) {
+			console.log("SLACK_BOT_TOKEN is not defined");
+			return process.exit(1);
+		}
+		if (!process.env.SLACK_FIRST_CHANNEL_ID) {
+			console.log("SLACK_FIRST_CHANNEL_ID is not defined");
+			return process.exit(1);
+		}
 
-    const options = new Options();
-    options.addArguments(
-      `--user-data-dir=/tmp/chrome_${Date.now()}_${Math.random()
-        .toString(36)
-        .substring(2, 11)}`
-    );
-    options.addArguments("--headless");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--disable-gpu");
-    // options.addArguments("--disable-blink-features=AutomationControlled");
-    // options.addArguments("--disable-extensions");
-    // options.addArguments("--no-first-run");
-    // options.addArguments("--disable-default-apps");
-    options.addArguments(
-      "--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-    );
-    // options.excludeSwitches("enable-automation");
-    // options.addArguments("--disable-web-security");
-    // options.addArguments("--allow-running-insecure-content");
+		const options = new Options();
+		options.addArguments(
+			`--user-data-dir=/tmp/chrome_${Date.now()}_${Math.random()
+				.toString(36)
+				.substring(2, 11)}`,
+		);
+		options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-gpu");
+		// options.addArguments("--disable-blink-features=AutomationControlled");
+		// options.addArguments("--disable-extensions");
+		// options.addArguments("--no-first-run");
+		// options.addArguments("--disable-default-apps");
+		options.addArguments(
+			"--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+		);
+		// options.excludeSwitches("enable-automation");
+		// options.addArguments("--disable-web-security");
+		// options.addArguments("--allow-running-insecure-content");
 
-    this.driver = new Builder()
-      .forBrowser("chrome")
-      .setChromeOptions(options)
-      .build();
-  }
+		this.driver = new Builder()
+			.forBrowser("chrome")
+			.setChromeOptions(options)
+			.build();
+	}
 
-  async scrapeJobs(): Promise<Prisma.SafeJobCreateInput[]> {
-    await this.driver.get("https://jobs.lever.co/safe");
-    const listGroups = await this.driver.findElements(
-      By.xpath("//div[@class='postings-group']")
-    );
-    let index = 0;
-    const data: Prisma.SafeJobCreateInput[] = [];
-    let groupName = "";
-    if (listGroups.length === 0) {
-      console.log("No job postings found.");
-      return data;
-    }
-    while (true) {
-      const group = listGroups[index];
-      const checkGroup = await group.findElements(
-        By.xpath(".//div[@class='large-category-header']")
-      );
-      if (checkGroup.length !== 0) {
-        groupName = await checkGroup[0].getText();
-      }
-      const teamName = await group
-        .findElement(
-          By.xpath(".//div[contains(@class, 'posting-category-title')]")
-        )
-        .getText();
-      const jobElements = await group.findElements(
-        By.xpath(".//div[@class='posting']")
-      );
-      for (const jobElement of jobElements) {
-        const href = await jobElement
-          .findElement(By.css("a"))
-          .getAttribute("href");
-        const title = await jobElement.findElement(By.css("h5")).getText();
-        const workplaceType = (
-          await jobElement
-            .findElement(
-              By.xpath(".//span[contains(@class, 'workplaceTypes')]")
-            )
-            .getText()
-        ).replace(" — ", "");
-        const employmentType = await jobElement
-          .findElement(By.xpath(".//span[contains(@class, 'commitment')]"))
-          .getText();
-        const location = await jobElement
-          .findElement(By.xpath(".//span[contains(@class, 'location')]"))
-          .getText();
-        data.push({
-          group: groupName,
-          department: teamName,
-          title,
-          href,
-          location,
-          workplaceType,
-          employmentType,
-        });
-      }
-      index++;
-      if (index >= listGroups.length) {
-        break;
-      }
-    }
-    return data;
-  }
+	async scrapeJobs(): Promise<Prisma.SafeJobCreateInput[]> {
+		await this.driver.get("https://jobs.lever.co/safe");
+		const listGroups = await this.driver.findElements(
+			By.xpath("//div[@class='postings-group']"),
+		);
+		let index = 0;
+		const data: Prisma.SafeJobCreateInput[] = [];
+		let groupName = "";
+		if (listGroups.length === 0) {
+			console.log("No job postings found.");
+			return data;
+		}
+		while (true) {
+			const group = listGroups[index];
+			const checkGroup = await group.findElements(
+				By.xpath(".//div[@class='large-category-header']"),
+			);
+			if (checkGroup.length !== 0) {
+				groupName = await checkGroup[0].getText();
+			}
+			const teamName = await group
+				.findElement(
+					By.xpath(".//div[contains(@class, 'posting-category-title')]"),
+				)
+				.getText();
+			const jobElements = await group.findElements(
+				By.xpath(".//div[@class='posting']"),
+			);
+			for (const jobElement of jobElements) {
+				const href = await jobElement
+					.findElement(By.css("a"))
+					.getAttribute("href");
+				const title = await jobElement.findElement(By.css("h5")).getText();
+				const workplaceType = (
+					await jobElement
+						.findElement(
+							By.xpath(".//span[contains(@class, 'workplaceTypes')]"),
+						)
+						.getText()
+				).replace(" — ", "");
+				const employmentType = await jobElement
+					.findElement(By.xpath(".//span[contains(@class, 'commitment')]"))
+					.getText();
+				const location = await jobElement
+					.findElement(By.xpath(".//span[contains(@class, 'location')]"))
+					.getText();
+				data.push({
+					group: groupName,
+					department: teamName,
+					title,
+					href,
+					location,
+					workplaceType,
+					employmentType,
+				});
+			}
+			index++;
+			if (index >= listGroups.length) {
+				break;
+			}
+		}
+		return data;
+	}
 
-  async filterData(data: Prisma.SafeJobCreateInput[]) {
-    const filteredData = await this.db.compareData(data);
-    const listDeleteId = [
-      ...filteredData.deleteJobs.map((job) => job.id as string),
-      ...filteredData.updateJobs.map((job) => job.id as string),
-    ];
-    const listCreateData = [
-      ...filteredData.newJobs,
-      ...filteredData.updateJobs.map((job) => {
-        const { id, ...jobData } = job;
-        return jobData;
-      }),
-    ];
-    await this.db.deleteMany(listDeleteId);
-    await this.db.createMany(listCreateData);
-    return filteredData;
-  }
+	async filterData(data: Prisma.SafeJobCreateInput[]) {
+		const filteredData = await this.db.compareData(data);
+		const listDeleteId = [
+			...filteredData.deleteJobs.map((job) => job.id as string),
+			...filteredData.updateJobs.map((job) => job.id as string),
+		];
+		const listCreateData = [
+			...filteredData.newJobs,
+			...filteredData.updateJobs.map((job) => {
+				const { id, ...jobData } = job;
+				return jobData;
+			}),
+		];
+		await this.db.deleteMany(listDeleteId);
+		await this.db.createMany(listCreateData);
+		return filteredData;
+	}
 
-  async sendMessage(data: {
-    newJobs: Prisma.SafeJobCreateInput[];
-    updateJobs: Prisma.SafeJobCreateInput[];
-    deleteJobs: Prisma.SafeJobCreateInput[];
-  }) {
-    const blocks = buildLeverJobMessage(data, "Safe", "https://safe.security/");
-    return { blocks, channel: 1 };
-  }
+	async sendMessage(data: {
+		newJobs: Prisma.SafeJobCreateInput[];
+		updateJobs: Prisma.SafeJobCreateInput[];
+		deleteJobs: Prisma.SafeJobCreateInput[];
+	}) {
+		const jobs: JobMessageData[] = data.newJobs.map((jobData) => {
+			return {
+				location: jobData.location,
+				title: jobData.title,
+				href: jobData.href,
+			};
+		});
 
-  async close() {
-    await this.driver.quit();
-  }
+		const blocks = buildJobMessage(jobs, "Safe", "https://safe.security/", 1);
+		return { blocks, channel: 1 };
+	}
 
-  static async run() {
-    const handler = new SafeJobHandler();
-    const jobData = await handler.scrapeJobs();
-    const filteredData = await handler.filterData(jobData);
-    if (
-      filteredData.newJobs.length === 0 &&
-      filteredData.updateJobs.length === 0 &&
-      filteredData.deleteJobs.length === 0
-    ) {
-      console.log("No job changes detected.");
-      await handler.close();
-      return { blocks: [] as any[], channel: 0 };
-    }
-    await handler.close();
-    return await handler.sendMessage(filteredData);
-  }
+	async close() {
+		await this.driver.quit();
+	}
+
+	static async run() {
+		const handler = new SafeJobHandler();
+		const jobData = await handler.scrapeJobs();
+		const filteredData = await handler.filterData(jobData);
+		if (
+			filteredData.newJobs.length === 0 &&
+			filteredData.updateJobs.length === 0 &&
+			filteredData.deleteJobs.length === 0
+		) {
+			console.log("No job changes detected.");
+			await handler.close();
+			return { blocks: [] as any[], channel: 0 };
+		}
+		await handler.close();
+		return await handler.sendMessage(filteredData);
+	}
 }
