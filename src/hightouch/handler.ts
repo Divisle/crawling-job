7b8a02c8f2ec9164e9f56c84e448486a34d1f6b9
PlanHorror <nmtxma2004@gmail.com>
2025-09-17 16:10:32 +0700

    refactor: refactor job message (Hightouch)

--- src/hightouch/handler.ts
+++ src/hightouch/handler.ts
@@ -2,7 +2,12 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { Builder, By, WebDriver } from "selenium-webdriver";
 import { Options } from "selenium-webdriver/chrome.js";
 import { HightouchJobRepository } from "./database";
-import { buildDefaultJobMessage, DefaultJobMessageData } from "../template";
+import {
+  buildDefaultJobMessage,
+  buildJobMessage,
+  DefaultJobMessageData,
+  JobMessageData,
+} from "../template";
 import { buildMessage } from "../global";
 
 export class HightouchJobScraper {
@@ -109,10 +114,16 @@ export class HightouchJobScraper {
   }
 
   async sendMessage(data: DefaultJobMessageData) {
-    const blocks = buildDefaultJobMessage(
-      data,
+    const jobDatas: JobMessageData[] = data.newJobs.map((job) => ({
+      location: job.location,
+      title: job.title,
+      href: job.href,
+    }));
+    const blocks = buildJobMessage(
+      jobDatas,
       "Hightouch",
-      "https://hightouch.com/"
+      "https://hightouch.com/",
+      1
     );
     return { blocks, channel: 1 };
   }
