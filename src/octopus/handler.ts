11cddb494851ffebf532b5e3504e3ad2e92aae6d
PlanHorror <nmtxma2004@gmail.com>
2025-10-10 19:01:46 +0700

    feat: octopus web scraper

---
+++ src/octopus/handler.ts
@@ -0,0 +1,96 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { OctopusJobRepository } from "./database";
+import { JobMessageData, buildJobMessage } from "../template";
+import axios from "axios";
+import { buildMessage } from "../global";
+
+export class OctopusJobHandler {
+  constructor(private db = new OctopusJobRepository(new PrismaClient())) {
+    if (!process.env.SLACK_BOT_TOKEN) {
+      console.log("SLACK_BOT_TOKEN is not defined");
+      return process.exit(1);
+    }
+    if (!process.env.SLACK_FIRST_CHANNEL_ID) {
+      console.log("SLACK_FIRST_CHANNEL_ID is not defined");
+      return process.exit(1);
+    }
+  }
+
+  async scrapeJobs(): Promise<Prisma.OctopusJobCreateInput[]> {
+    try {
+      const response: {
+        data: {
+          categories: { location: string };
+          text: string;
+          hostedUrl: string;
+          id: string;
+        }[];
+      } = await axios.get("https://api.lever.co/v0/postings/octopus?mode=json");
+      const data: Prisma.OctopusJobCreateInput[] = response.data.map((job) => ({
+        title: job.text,
+        location: job.categories.location || "No Location",
+        href: "https://octopus.com/company/careers/" + job.id,
+      }));
+      console.log(`Scraped ${data.length} jobs from Octopus`);
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Error scraping jobs:", error);
+      return [];
+    }
+  }
+
+  async filterData(
+    jobData: Prisma.OctopusJobCreateInput[]
+  ): Promise<JobMessageData[]> {
+    const filterData = await this.db.compareData(jobData);
+    const listDeleteId = [
+      ...filterData.deleteJobs.map((job) => job.id!),
+      ...filterData.updateJobs.map((job) => job.id!),
+    ];
+    const listCreateData = [
+      ...filterData.newJobs,
+      ...filterData.updateJobs.map((job) => ({
+        title: job.title,
+        location: job.location,
+        href: job.href,
+      })),
+    ];
+    if (listDeleteId.length !== 0) {
+      await this.db.deleteMany(listDeleteId);
+    }
+    if (listCreateData.length !== 0) {
+      await this.db.createMany(listCreateData);
+    }
+    return filterData.newJobs.map((job) => ({
+      location: job.location,
+      title: job.title,
+      href: job.href,
+    }));
+  }
+
+  async sendMessage(data: JobMessageData[]) {
+    const blocks = buildJobMessage(data, "Octopus", "https://octopus.com/", 1);
+    return {
+      blocks,
+      channel: 1,
+    };
+  }
+
+  static async run() {
+    const handler = new OctopusJobHandler();
+    const data = await handler.scrapeJobs();
+    const filteredData = await handler.filterData(data);
+    if (filteredData.length === 0) {
+      console.log("No job changes detected.");
+      return { blocks: [] as any[], channel: 0 };
+    }
+    return await handler.sendMessage(filteredData);
+  }
+}
+
+OctopusJobHandler.run().then((res) => {
+  if (res.blocks.length > 0) {
+    buildMessage(res.channel, res.blocks);
+  }
+});
