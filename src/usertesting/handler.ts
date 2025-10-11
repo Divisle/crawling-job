ea53f853f3cc48024650a50af35fc8e049fa390b
PlanHorror <nmtxma2004@gmail.com>
2025-10-11 09:43:44 +0700

    feat: usertesting web scraper

--- src/usertesting/handler.ts
+++ src/usertesting/handler.ts
@@ -1,11 +1,11 @@
 import { Prisma, PrismaClient } from "@prisma/client";
-import { SnorkelAIJobRepository } from "./database";
+import { UserTestingJobRepository } from "./database";
 import { JobMessageData, buildJobMessage } from "../template";
 import axios from "axios";
 import { buildMessage } from "../global";
 
-export class SnorkelAIJobHandler {
-  constructor(private db = new SnorkelAIJobRepository(new PrismaClient())) {
+export class UserTestingJobHandler {
+  constructor(private db = new UserTestingJobRepository(new PrismaClient())) {
     if (!process.env.SLACK_BOT_TOKEN) {
       console.log("SLACK_BOT_TOKEN is not defined");
       return process.exit(1);
@@ -16,29 +16,35 @@ export class SnorkelAIJobHandler {
     }
   }
 
-  async scrapeJobs(): Promise<Prisma.SnorkelAIJobCreateInput[]> {
+  async scrapeJobs(): Promise<Prisma.UserTestingJobCreateInput[]> {
     try {
+      const payload = {
+        appliedFacets: {},
+        limit: 20,
+        offset: 0,
+        searchText: "",
+      };
       const response: {
         data: {
-          jobs: {
+          jobPostings: {
             title: string;
-            absolute_url: string;
-            location: {
-              name: string;
-            };
+            locationsText: string;
+            externalPath: string;
           }[];
         };
-      } = await axios.get(
-        "https://boards-api.greenhouse.io/v1/boards/snorkelai/jobs"
+      } = await axios.post(
+        "https://usertesting.wd12.myworkdayjobs.com/wday/cxs/usertesting/UserTesting/jobs",
+        payload
       );
-      const data: Prisma.SnorkelAIJobCreateInput[] = response.data.jobs.map(
-        (job) => ({
+      const data: Prisma.UserTestingJobCreateInput[] =
+        response.data.jobPostings.map((job) => ({
           title: job.title,
-          location: job.location.name || "No Location",
-          href: job.absolute_url,
-        })
-      );
-      console.log(`Scraped ${data.length} jobs from SnorkelAI`);
+          location: job.locationsText || "No Location",
+          href:
+            "https://usertesting.wd12.myworkdayjobs.com/en-US/UserTesting" +
+            job.externalPath,
+        }));
+      console.log(`Scraped ${data.length} jobs from UserTesting`);
       console.log(data);
       return data;
     } catch (error) {
@@ -48,7 +54,7 @@ export class SnorkelAIJobHandler {
   }
 
   async filterData(
-    jobData: Prisma.SnorkelAIJobCreateInput[]
+    jobData: Prisma.UserTestingJobCreateInput[]
   ): Promise<JobMessageData[]> {
     const filterData = await this.db.compareData(jobData);
     const listDeleteId = [
@@ -79,8 +85,8 @@ export class SnorkelAIJobHandler {
   async sendMessage(data: JobMessageData[]) {
     const blocks = buildJobMessage(
       data,
-      "SnorkelAI",
-      "https://www.snorkel.ai/",
+      "UserTesting",
+      "https://www.usertesting.com/",
       1
     );
     return {
@@ -90,7 +96,7 @@ export class SnorkelAIJobHandler {
   }
 
   static async run() {
-    const handler = new SnorkelAIJobHandler();
+    const handler = new UserTestingJobHandler();
     const data = await handler.scrapeJobs();
     const filteredData = await handler.filterData(data);
     if (filteredData.length === 0) {
@@ -101,8 +107,8 @@ export class SnorkelAIJobHandler {
   }
 }
 
-// SnorkelAIJobHandler.run().then((res) => {
-//   if (res.blocks.length > 0) {
-//     buildMessage(res.channel, res.blocks);
-//   }
-// });
+UserTestingJobHandler.run().then((res) => {
+  if (res.blocks.length > 0) {
+    buildMessage(res.channel, res.blocks);
+  }
+});
