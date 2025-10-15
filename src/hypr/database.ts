8dcbd19ee7775be8418cbe5b7b72c1376edb9a2c
PlanHorror <nmtxma2004@gmail.com>
2025-10-15 14:01:36 +0700

    feat: hybr web scraper

---
+++ src/hypr/database.ts
@@ -0,0 +1,80 @@
+import { HyprJob, Prisma, PrismaClient } from "@prisma/client";
+
+export class HyprJobRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  async getAll(): Promise<HyprJob[]> {
+    return this.prisma.hyprJob.findMany({});
+  }
+
+  async createMany(data: Prisma.HyprJobCreateInput[]): Promise<boolean> {
+    try {
+      await this.prisma.hyprJob.createMany({
+        data,
+      });
+      return true;
+    } catch (error) {
+      console.error("Error creating HYPR jobs:", error);
+      return false;
+    }
+  }
+
+  async deleteMany(ids: string[]): Promise<boolean> {
+    try {
+      await this.prisma.hyprJob.deleteMany({
+        where: {
+          id: {
+            in: ids,
+          },
+        },
+      });
+      return true;
+    } catch (error) {
+      console.error("Error deleting HYPR jobs:", error);
+      return false;
+    }
+  }
+
+  async compareData(data: Prisma.HyprJobCreateInput[]) {
+    const deleteJobs: Prisma.HyprJobCreateInput[] = [];
+    const updateJobs: Prisma.HyprJobCreateInput[] = [];
+    const newJobs: Prisma.HyprJobCreateInput[] = [];
+    const existingJobs = await this.getAll();
+
+    data.forEach((job) => {
+      const existingJob = existingJobs.find((j) => j.href === job.href);
+      if (existingJob) {
+        if (
+          existingJob.title === job.title &&
+          existingJob.location === job.location
+        ) {
+        } else {
+          updateJobs.push({
+            id: existingJob.id,
+            title: job.title,
+            location: job.location,
+            href: job.href,
+          });
+        }
+      } else {
+        newJobs.push({
+          title: job.title,
+          location: job.location,
+          href: job.href,
+        });
+      }
+    });
+    existingJobs.forEach((job) => {
+      const locExists = data.find((j) => j.href === job.href);
+      if (!locExists) {
+        deleteJobs.push({
+          id: job.id,
+          title: job.title,
+          location: job.location,
+          href: job.href,
+        });
+      }
+    });
+    return { deleteJobs, updateJobs, newJobs };
+  }
+}
---
+++ src/hypr/handler.ts
@@ -0,0 +1,103 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { HyprJobRepository } from "./database";
+import { JobMessageData, buildJobMessage } from "../template";
+import axios from "axios";
+import { buildMessage } from "../global";
+
+export class HyprJobHandler {
+  constructor(private db = new HyprJobRepository(new PrismaClient())) {
+    if (!process.env.SLACK_BOT_TOKEN) {
+      console.log("SLACK_BOT_TOKEN is not defined");
+      return process.exit(1);
+    }
+    if (!process.env.SLACK_FIRST_CHANNEL_ID) {
+      console.log("SLACK_FIRST_CHANNEL_ID is not defined");
+      return process.exit(1);
+    }
+  }
+
+  async scrapeJobs(): Promise<Prisma.HyprJobCreateInput[]> {
+    try {
+      const response: {
+        data: {
+          jobs: {
+            title: string;
+            absolute_url: string;
+            location: {
+              name: string;
+            };
+          }[];
+        };
+      } = await axios.get(
+        "https://boards-api.greenhouse.io/v1/boards/hypr/jobs"
+      );
+      const data: Prisma.HyprJobCreateInput[] = response.data.jobs.map(
+        (job) => ({
+          title: job.title,
+          location: job.location.name || "No Location",
+          href: job.absolute_url,
+        })
+      );
+      // console.log(`Scraped ${data.length} jobs from HYPR`);
+      // console.log(data);
+      return data;
+    } catch (error) {
+      console.error("Error scraping jobs:", error);
+      return [];
+    }
+  }
+
+  async filterData(
+    jobData: Prisma.HyprJobCreateInput[]
+  ): Promise<JobMessageData[]> {
+    const filterData = await this.db.compareData(jobData);
+    const listDeleteId = [
+      ...filterData.deleteJobs.map((job) => job.id!),
+      ...filterData.updateJobs.map((job) => job.id!),
+    ];
+    const listCreateData = [
+      ...filterData.newJobs,
+      ...filterData.updateJobs.map((job) => ({
+        title: job.title,
+        location: job.location,
+        href: job.href,
+      })),
+    ];
+    if (listDeleteId.length !== 0) {
+      await this.db.deleteMany(listDeleteId);
+    }
+    if (listCreateData.length !== 0) {
+      await this.db.createMany(listCreateData);
+    }
+    return filterData.newJobs.map((job) => ({
+      location: job.location,
+      title: job.title,
+      href: job.href,
+    }));
+  }
+
+  async sendMessage(data: JobMessageData[]) {
+    const blocks = buildJobMessage(data, "HYPR", "https://www.hypr.com/", 2);
+    return {
+      blocks,
+      channel: 2,
+    };
+  }
+
+  static async run() {
+    const handler = new HyprJobHandler();
+    const data = await handler.scrapeJobs();
+    const filteredData = await handler.filterData(data);
+    if (filteredData.length === 0) {
+      console.log("No job changes detected.");
+      return { blocks: [] as any[], channel: 0 };
+    }
+    return await handler.sendMessage(filteredData);
+  }
+}
+
+// HyprJobHandler.run().then((res) => {
+//   if (res.blocks.length > 0) {
+//     buildMessage(res.channel, res.blocks);
+//   }
+// });
