666c3b47e0fad4c93736ea9e452341dfc0867ccb
PlanHorror <nmtxma2004@gmail.com>
2026-01-18 13:13:47 +0700

    feat: acceldata web scraper

---
+++ src/acceldata/database.ts
@@ -0,0 +1,80 @@
+import { AcceldataJob, Prisma, PrismaClient } from "@prisma/client";
+
+export class AcceldataJobRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  async getAll(): Promise<AcceldataJob[]> {
+    return this.prisma.acceldataJob.findMany({});
+  }
+
+  async createMany(data: Prisma.AcceldataJobCreateInput[]): Promise<boolean> {
+    try {
+      await this.prisma.acceldataJob.createMany({
+        data,
+      });
+      return true;
+    } catch (error) {
+      console.error("Error creating Acceldata jobs:", error);
+      return false;
+    }
+  }
+
+  async deleteMany(ids: string[]): Promise<boolean> {
+    try {
+      await this.prisma.acceldataJob.deleteMany({
+        where: {
+          id: {
+            in: ids,
+          },
+        },
+      });
+      return true;
+    } catch (error) {
+      console.error("Error deleting Acceldata jobs:", error);
+      return false;
+    }
+  }
+
+  async compareData(data: Prisma.AcceldataJobCreateInput[]) {
+    const deleteJobs: Prisma.AcceldataJobCreateInput[] = [];
+    const updateJobs: Prisma.AcceldataJobCreateInput[] = [];
+    const newJobs: Prisma.AcceldataJobCreateInput[] = [];
+    const existingJobs = await this.getAll();
+
+    data.forEach((job) => {
+      const existingJob = existingJobs.find((j) => j.href === job.href);
+      if (existingJob) {
+        if (
+          existingJob.title === job.title &&
+          existingJob.location === job.location
+        ) {
+        } else {
+          updateJobs.push({
+            id: existingJob.id,
+            title: job.title,
+            location: job.location,
+            href: job.href,
+          });
+        }
+      } else {
+        newJobs.push({
+          title: job.title,
+          location: job.location,
+          href: job.href,
+        });
+      }
+    });
+    existingJobs.forEach((job) => {
+      const locExists = data.find((j) => j.href === job.href);
+      if (!locExists) {
+        deleteJobs.push({
+          id: job.id,
+          title: job.title,
+          location: job.location,
+          href: job.href,
+        });
+      }
+    });
+    return { deleteJobs, updateJobs, newJobs };
+  }
+}
